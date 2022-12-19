dc5eeec6c15b2b85581c2f416c166c6d85ef6e02
daddyyankky <michaelokoro567@gmail.com>
2022-12-19 08:36:51 -0800

    update

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ * _putchar - writers characters c to stdout
+ * @c: char to be printed
+ * Return: On sucess 1
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1);       
+}}			
