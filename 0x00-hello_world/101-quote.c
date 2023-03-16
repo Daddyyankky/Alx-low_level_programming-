e36f062ce9714476b3c3601bbe11ce5d65bd5e2a
daddyyankky <michaelokoro567@gmail.com>
2023-03-16 12:55:12 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdio.h>
+/**
+ * main - A c program that prints a line to the stardard eror
+ * Retrun: 1 (success)
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 20 15-10-19\n", 59);
+	return (1);
+}
