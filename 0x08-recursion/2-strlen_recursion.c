52bd823b89f6b16a635354d36a8c35f707ed6876
daddyyankky <michaelokoro567@gmail.com>
2023-04-05 21:31:21 -0700

    task 3

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,19 +1,18 @@
-nclude "main.h"
-
+#include "main.h"
 /**
- * _strlen_recursion - returns the length of a string.
- * @s: input string
- * Return: len
+ * _strlen_recursion - length of string
+ * @s:string
+ * Return:int
  */
-
 int _strlen_recursion(char *s)
 {
-	int len = 0;
+	int i = 0;
 
-	if (*s != '\0')
+	if (*s)
 	{
-		len++;
-		len += _strlen_recursion(s + 1);
+		i++;
+ 	        i += _strlen_recursion(s + 1);
+
 	}
-	return (len);
+	return (i);
 }
